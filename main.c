594ea46680949a5f5373c11670d9b23bbe48a46e
Aidan Nunn <aidan.nunn@wsu.edu>
2022-04-04 16:53:17 -0700

    Added creat() function

--- main.c
+++ main.c
@@ -66,7 +66,7 @@ int main(int argc, char *argv[ ])
   // WRTIE code here to create P1 as a USER process
   
   while(1){
-    printf("\ninput command : [ls|cd|pwd|mkdir|rmdir|quit] ");
+    printf("\ninput command : [ls|cd|pwd|mkdir|creat|rmdir|quit] ");
     fgets(line, 128, stdin);
     line[strlen(line)-1] = 0;
 
@@ -85,6 +85,8 @@ int main(int argc, char *argv[ ])
       mypwd(running->cwd);
     else if (strcmp(cmd, "mkdir")==0)
       mymkdir();
+    else if (strcmp(cmd, "creat")==0)
+      mycreat();
     else if (strcmp(cmd, "rmdir")==0)
       myrmdir();
     else if (strcmp(cmd, "quit")==0)
